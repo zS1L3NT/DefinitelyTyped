ab4efc375a7c741d25b74f9c2eb6a159a062ec84
Zechariah <zechariahtan144@gmail.com>
2022-02-12 04:16:33 +0800

    Copied all JSDoc from the javascript files

--- types/socket.io-mock/socket-client.d.ts
+++ types/socket.io-mock/socket-client.d.ts
@@ -1,15 +1,30 @@
 import SocketMock from '.';
-import Emitter from "../component-emitter"
+import Emitter from '../component-emitter';
 
 declare class SocketClient extends Emitter {
     public connected: boolean;
     public disconnected: boolean;
 
+    /**
+     * A mocking class for the Socket IO Client side
+     * @param {SocketMock} socketMock
+     */
     public constructor(socketMock: SocketMock);
 
+    /**
+     * Fire an event to the server
+     * @param  {string}   eventKey -- The event key that needs to be attached
+     * @param  {any}   payload -- The payload that needs to be attached to the emit
+     */
     public fireEvent(eventKey: string, payload: any): void;
 
+    /**
+     * Close the socket
+     */
     public close(): this;
 
+    /**
+     * Disconnet the socket alias for close
+     */
     public disconnect(): this;
 }
