90e2b9af3645af77daf7ea52040dce68417f0121
Zechariah <zechariahtan144@gmail.com>
2022-02-12 04:07:49 +0800

    Changed export default to declaration

--- types/socket.io-mock/socket-client.d.ts
+++ types/socket.io-mock/socket-client.d.ts
@@ -1,7 +1,7 @@
 import SocketMock from '.';
 import Emitter from "../component-emitter"
 
-export default class SocketClient extends Emitter {
+declare class SocketClient extends Emitter {
     public connected: boolean;
     public disconnected: boolean;
 
